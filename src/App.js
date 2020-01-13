d2de8de58cbb70bdfa25fa17acb025d2c22fa349
Joao Lucas <jlmelosouza18@gmail.com>
2020-01-13 14:07:47 -0300

    feat: routes and styles

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+
+import Routes from './routes';
+import GlobalStyles from './styles/global';
 
 function App() {
-  return <h1>Hello Rocketshoes</h1>;
+  return (
+    <BrowserRouter>
+      <GlobalStyles />
+      <Routes />
+    </BrowserRouter>
+  );
 }
 
 export default App;
