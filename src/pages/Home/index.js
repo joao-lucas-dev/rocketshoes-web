d6d989d1750f8ba43af85f8e2ae8c45480e90add
Joao Lucas <jlmelosouza18@gmail.com>
2020-01-19 13:05:52 -0300

    fix: turn Home into class

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,45 +1,47 @@
-import React, { useState, useEffect } from 'react';
+import React, { Component } from 'react';
 import { MdAddShoppingCart } from 'react-icons/md';
 
 import api from '../../services/api';
 import { formatPrice } from '../../utils/format';
 import { ProductList } from './styles';
 
-export default function Home() {
-  const [products, setProducts] = useState([]);
+export default class Home extends Component {
+  state = {
+    products: [],
+  };
 
-  useEffect(() => {
-    async function loadData() {
-      const response = await api.get('/products');
+  async componentDidMount() {
+    const response = await api.get('/products');
 
-      const data = response.data.map(product => ({
-        ...product,
-        priceFormatted: formatPrice(product.price),
-      }));
+    const data = response.data.map(product => ({
+      ...product,
+      priceFormatted: formatPrice(product.price),
+    }));
 
-      setProducts(data);
-    }
+    this.setState({ products: data });
+  }
 
-    loadData();
-  }, []);
+  render() {
+    const { products } = this.state;
 
-  return (
-    <ProductList>
-      {products.map(product => (
-        <li key={product.id}>
-          <img src={product.image} alt="Tênis" />
-          <strong>{product.title}</strong>
-          <span>{product.priceFormatted}</span>
+    return (
+      <ProductList>
+        {products.map(product => (
+          <li key={product.id}>
+            <img src={product.image} alt="Tênis" />
+            <strong>{product.title}</strong>
+            <span>{product.priceFormatted}</span>
 
-          <button type="button">
-            <div>
-              <MdAddShoppingCart size={16} color="#FFF" /> 3
-            </div>
+            <button type="button">
+              <div>
+                <MdAddShoppingCart size={16} color="#FFF" /> 3
+              </div>
 
-            <span>ADICIONAR AO CARRINHO</span>
-          </button>
-        </li>
-      ))}
-    </ProductList>
-  );
+              <span>ADICIONAR AO CARRINHO</span>
+            </button>
+          </li>
+        ))}
+      </ProductList>
+    );
+  }
 }
