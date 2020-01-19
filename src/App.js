12e211e1bd17c703e5286184a15acc3d6edfa5bf
Joao Lucas <jlmelosouza18@gmail.com>
2020-01-19 12:58:43 -0300

    feat: redux config

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 
 import Header from './components/Header';
 import Routes from './routes';
+import store from './store';
 import GlobalStyles from './styles/global';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Header />
-      <Routes />
-      <GlobalStyles />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Header />
+        <Routes />
+        <GlobalStyles />
+      </BrowserRouter>
+    </Provider>
   );
 }
 
